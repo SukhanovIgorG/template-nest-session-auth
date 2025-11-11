445f12cb4f341866eba18c49f7b0b4b635034771
SukhanovIgorG <sukhanovgarik@yandex.ru>
2025-11-11 17:54:10 +0300

    feat(api): add swagger documentation support
    Integrate @nestjs/swagger package and configure OpenAPI documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //swagger settings
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('The BAS API description')
+    .setVersion('1.0')
+    .addTag('BAS')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
   // Disable CORS for all origins
   app.enableCors({
     origin: 'http://localhost:5173',
